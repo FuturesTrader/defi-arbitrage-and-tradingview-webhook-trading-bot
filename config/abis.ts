924e56ef26de7bf61876347530bc25948831ddcd
Todd Griggs <rtgriggs@gmail.com>
2025-09-08 23:42:33 -0500

    Remove trade_data/ and transaction_data/ from tracking
    - Updated .gitignore to exclude data directories
    - Removed trade_data/ and transaction_data/ from Git tracking
    - These directories will remain local but won't be synced to GitHub
    - Protects sensitive trading and transaction data

--- config/abis.ts
+++
@@ -1,9 +0,0 @@
-export const wagmiAbi = [
-        {
-            inputs: [{ name: "owner", type: "uint32" }],
-            name: "mint",
-            outputs: [{ name: "", type: "uint32" }],
-            stateMutability: "view",
-            type: "function",
-        },
-] as const;
